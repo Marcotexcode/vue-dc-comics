944f9fe074bce90bad13472282867ecce0cd6836
Marco Tessitore <marcotexcode@gmail.com>
2021-07-06 14:02:47 +0200

    add header

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+
+    <h1>hello</h1>
+
+</template>
+
+
+<script>
+export default {
+
+    name: 'Header',
+
+    data() {
+
+        
+
+    }
+
+}
+</script>
+
+
+<style lang="scss" scope>
+
+</style>
