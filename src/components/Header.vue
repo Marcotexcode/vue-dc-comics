c4741912a76f86e0e882997676db51f8af392932
Marco Tessitore <marcotexcode@gmail.com>
2021-07-06 14:18:01 +0200

    add header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,27 @@
 <template>
 
-    <h1>hello</h1>
+    <header>
+
+        <img src="../assets/img/dc-logo.png" alt="">
+
+        <nav>
+
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Home</a></li>
+            </ul>
+
+        </nav> 
+
+    </header>
 
 </template>
 
@@ -12,8 +33,6 @@ export default {
 
     data() {
 
-        
-
     }
 
 }
@@ -21,5 +40,32 @@ export default {
 
 
 <style lang="scss" scope>
+
+    header {
+        display: flex;
+        justify-content: space-around;
+        border: 1px solid #000;
+
+        ul {
+            display: flex;
+            align-items: center;
+            
+            li{
+                padding: 20px;
+                list-style: none;
+
+                a {
+                    color: black;
+                    text-decoration: none;
+
+                    &:hover {
+                        color: blue;
+                        border-bottom: 3px solid blue;
+                        padding-bottom: 100%;
+                    }
+                }
+            }
+        }
+    }
 
 </style>
