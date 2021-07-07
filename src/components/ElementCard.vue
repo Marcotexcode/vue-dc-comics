a2a5afc10b87cf897215c9570f0e4ceecc886256
Marco Tessitore <marcotexcode@gmail.com>
2021-07-07 15:15:46 +0200

    inizio sezione 1

---
+++ src/components/ElementCard.vue
@@ -0,0 +1,44 @@
+<template>
+
+        <div class="box">
+            <img :src="dettagli.thumb" :alt="dettagli.series">
+            <span>{{dettagli.series}}</span>
+        </div>
+
+</template>
+
+
+<script>
+
+export default {
+
+    name: 'ElementCard',
+
+    props: ['dettagli']
+
+}
+
+</script>
+
+
+<style lang="scss" scoped>
+
+    .box {
+            width: 150px;
+            margin: 40px 20px;
+            height: 150px;
+
+        img {
+            width: 100%;
+            height: 100%;
+            display: block;
+        }
+
+        span {
+            margin: 10px 0px;
+            color: white;
+        }
+    }
+     
+
+</style>
