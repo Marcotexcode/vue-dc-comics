809e3fa422f9772cd1edd4c118e205c65b0fac99
Marco Tessitore <marcotexcode@gmail.com>
2021-07-07 16:16:57 +0200

    modifiche

--- src/App.vue
+++ src/App.vue
@@ -61,15 +61,6 @@
 
 <style lang="scss">
 
-@import '../src/style/commons.scss';
+  @import '../src/style/commons.scss';
 
-
-// #app {
-//   font-family: Avenir, Helvetica, Arial, sans-serif;
-//   -webkit-font-smoothing: antialiased;
-//   -moz-osx-font-smoothing: grayscale;
-//   text-align: center;
-//   color: #2c3e50;
-//   margin-top: 60px;
-// }
 </style>
